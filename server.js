86f998d784b4e2f566e50da3c8c412b36e6e9fe3
yashsanghani-apptware <yash@192.168.1.5>
2024-12-16 11:21:39 +0530

    add docker file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/get", (req, res) => {
-    res.send("Hello Yash!!!!!!!!!!!!!!!");
+    res.send("Hello Yash0000!!!!!!!!!!!!!!!");
 });
 
 
